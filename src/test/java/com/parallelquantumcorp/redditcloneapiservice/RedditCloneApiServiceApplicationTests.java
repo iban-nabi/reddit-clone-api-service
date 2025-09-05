054e8cb607e0a6bba22c040773a383934df3b1ab
Ivan Paguyo <ezrapaguyo09@gmail.com>
2025-09-05 19:04:38 +0800

    Initial Commit

---
+++ src/test/java/com/parallelquantumcorp/redditcloneapiservice/RedditCloneApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parallelquantumcorp.redditcloneapiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedditCloneApiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
